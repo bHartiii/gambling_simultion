48790f278c8d7aee2b0bb2bd4e4519d158b75c2e
bharti mali <malibharti5@gmail.com>
2020-11-23 16:12:24 +0530

    ADD : Bet Result method is created

--- GamblingSimulation.java
+++ GamblingSimulation.java
@@ -1,6 +1,22 @@
+import java.util.*;
 public class GamblingSimulation{
 	public static void main(String[] args){
 		int totalStake=100;
-		int everyGameAmount=1;
+		int bet=1;
+		betResult();
+	}
+	public static void betResult(){
+		Random random = new Random();
+		int result=random.nextInt(2);
+		switch(result){
+			case 1:
+				System.out.println("win");
+				break;
+			case 2:
+				System.out.println("loose");
+				break;
+			default:
+				break;
+		}
 	}
 }
